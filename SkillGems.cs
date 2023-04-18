2bbf9e3783ad09ff9d470e5c200214075978f028
Detective Squirrel <ratman7539@windowslive.com>
2023-04-18 20:30:38 +1200

    Initial commit

---
+++ SkillGems.cs
@@ -0,0 +1,145 @@
+﻿using ExileCore;
+using ExileCore.PoEMemory;
+using ExileCore.PoEMemory.Components;
+using ExileCore.Shared.Helpers;
+using Microsoft.VisualBasic.Devices;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vector2 = System.Numerics.Vector2;
+
+namespace SkillGems
+{
+    public class SkillGems : BaseSettingsPlugin<SkillGemsSettings>
+    {
+        private CancellationTokenSource _gemLevelingCts;
+        private Task _gemLevelingTask;
+        private Vector2 _mousePosition;
+
+        public override bool Initialise()
+        {
+            Input.RegisterKey(Settings.Run);
+            return true;
+        }
+
+        public void Enable()
+        {
+            _gemLevelingCts = new CancellationTokenSource();
+        }
+
+        public void Disable()
+        {
+            _gemLevelingCts.Cancel();
+        }
+
+        private void SetCursorPos(Vector2 v)
+        {
+            Input.SetCursorPos(GameController.Window.GetWindowRectangleTimeCache.TopLeft.ToVector2Num() + v);
+        }
+
+        private void SetCursorPos(Element e)
+        {
+            SetCursorPos(e.GetClientRectCache.ClickRandomNum(5, 5));
+        }
+
+        public override Job Tick()
+        {
+            if (!Input.IsKeyDown(Keys.A) || !PanelVisible())
+            {
+                _gemLevelingCts.Cancel();
+            }
+            else if (CanTick() && IsPlayerAlive() && AnythingToLevel() && PanelVisible() && _gemLevelingTask == null)
+            {
+                _mousePosition = Input.MousePositionNum;
+                _gemLevelingCts = new CancellationTokenSource();
+                _gemLevelingTask = Task.FromResult(BeginGemLevel(_gemLevelingCts.Token)).Unwrap();
+                _gemLevelingTask.ContinueWith((task) =>
+                {
+                    _gemLevelingTask = null;
+                    SetCursorPos(_mousePosition);
+                });
+            }
+
+            return null;
+        }
+
+        private async Task BeginGemLevel(CancellationToken cancellationToken)
+        {
+            List<Element> gemsToLvlUpElements = GetLevelableGems();
+
+            if (!gemsToLvlUpElements.Any()) return;
+
+            Element elementToClick = gemsToLvlUpElements
+                .SelectMany(e => e.Children)
+                .Where(e => (int)e.Height > 40 && (int)e.Height < 50)
+                .FirstOrDefault();
+
+            int ActionDelay = Settings.DelayBetweenEachMouseEvent.Value;
+            int GemDelay = Settings.DelayBetweenEachGemClick.Value;
+
+            if (Settings.AddPingIntoDelay.Value)
+            {
+                ActionDelay += GameController.IngameState.ServerData.Latency;
+                GemDelay += GameController.IngameState.ServerData.Latency;
+            }
+
+            SetCursorPos(elementToClick);
+            await Task.Delay(ActionDelay, cancellationToken);
+            Input.LeftDown();
+            await Task.Delay(ActionDelay, cancellationToken);
+            Input.LeftUp();
+            await Task.Delay(GemDelay, cancellationToken);
+
+            if (cancellationToken.IsCancellationRequested) return;
+        }
+
+        private bool PanelVisible()
+        {
+            return !(GameController.Game.IngameState.IngameUi.InventoryPanel.IsVisible
+                || GameController.Game.IngameState.IngameUi.Atlas.IsVisible
+                || GameController.Game.IngameState.IngameUi.TreePanel.IsVisible
+                || GameController.Game.IngameState.IngameUi.SyndicatePanel.IsVisible
+                || GameController.Game.IngameState.IngameUi.OpenRightPanel.IsVisible
+                || GameController.Game.IngameState.IngameUi.DelveWindow.IsVisible);
+        }
+
+        private bool CanTick()
+        {
+            return !GameController.IsLoading
+                && GameController.Game.IngameState.ServerData.IsInGame
+                && GameController.Player != null
+                && GameController.Player.Address != 0
+                && GameController.Player.IsValid
+                && GameController.Window.IsForeground();
+        }
+
+        private bool IsPlayerAlive()
+        {
+            return GameController.Game.IngameState.Data.LocalPlayer.GetComponent<Life>().CurHP > 0;
+        }
+
+        private bool AnythingToLevel()
+        {
+            return GetLevelableGems().Any();
+        }
+
+        private List<Element> GetLevelableGems()
+        {
+            List<Element> gemsToLevelUp = new List<Element>();
+
+            var possibleGemsToLvlUpElements = GameController.IngameState.IngameUi?.GemLvlUpPanel?.GemsToLvlUp;
+
+            if (possibleGemsToLvlUpElements != null && possibleGemsToLvlUpElements.Any())
+                foreach (Element possibleGemsToLvlUpElement in possibleGemsToLvlUpElements)
+                    foreach (Element elem in possibleGemsToLvlUpElement.Children)
+                        if (elem.Text != null && elem.Text.Contains("Click to level"))
+                            gemsToLevelUp.Add(possibleGemsToLvlUpElement);
+
+            return gemsToLevelUp;
+        }
+    }
+}
---
+++ SkillGemsSettings.cs
@@ -0,0 +1,22 @@
+﻿using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using System.Windows.Forms;
+
+namespace SkillGems
+{
+    public class SkillGemsSettings : ISettings
+    {
+        //Mandatory setting to allow enabling/disabling your plugin
+        public ToggleNode Enable { get; set; } = new ToggleNode(false);
+        public ToggleNode ReturnMouseToStart { get; set; } = new ToggleNode(true);
+        public ToggleNode AddPingIntoDelay { get; set; } = new ToggleNode(false);
+        public HotkeyNode Run { get; set; } = new HotkeyNode(Keys.A);
+        public RangeNode<int> DelayBetweenEachGemClick { get; set; } = new RangeNode<int>(20, 0, 1000);
+        public RangeNode<int> DelayBetweenEachMouseEvent { get; set; } = new RangeNode<int>(20, 0, 1000);
+
+        //Put all your settings here if you can.
+        //There's a bunch of ready-made setting nodes,
+        //nested menu support and even custom callbacks are supported.
+        //If you want to override DrawSettings instead, you better have a very good reason.
+    }
+}
